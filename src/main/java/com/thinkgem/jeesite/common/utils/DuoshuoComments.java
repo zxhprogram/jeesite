81fe3c3d80d0501ef21fa3e0d0ac0b340c024a3a
zhaoxiuhuan <545676500@qq.com>
2016-08-04 16:26:28 +0800

    添加一个查询文章评论数的方法

---
+++ src/main/java/com/thinkgem/jeesite/common/utils/DuoshuoComments.java
@@ -0,0 +1,13 @@
+package com.thinkgem.jeesite.common.utils;
+
+public class DuoshuoComments {
+	
+	/**
+	 * 根据参数去多说api那里去获取相关的信息
+	 * @return
+	 */
+   public static String findMyComments(){
+	   
+	   return null;
+   }
+}
